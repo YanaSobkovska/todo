5d053ad47fb58146afc484142456733456311c5d
Yana Sobkovska <yanasobkovska2010@gmail.com>
2023-08-15 21:32:49 +0300

    asd

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,153 +1,189 @@
 <template>
-  <div class="container">
-    <div class="task-list">
-      <div class="list-item" v-for="(task, index) in taskList" :key="index">
-        <div class="list-item-task">
-          <h3 class="task-name">
-            <template v-if="editModeTaskId === task.id">
-              <input v-model="task.name" class="edit-input"
-            /></template>
-            <template v-else>{{ task.name }}</template>
-          </h3>
-          <p class="task-description">
-            <template v-if="editModeTaskId === task.id">
-              <textarea
-                v-model="task.description"
-                class="edit-textarea"
-              ></textarea>
-            </template>
-            <template v-else>{{ task.description }}</template>
-          </p>
-        </div>
-        <div class="list-item-button">
-          <el-button
-            @click="doneTask(task)"
-            :type="checkTypeButton(task)"
-            icon="el-icon-check"
-            circle
-          ></el-button>
-          <el-button
-            @click="toggleEditMode(task.id)"
-            :type="editModeTaskId === task.id ? 'primary' : ''"
-            :icon="
-              editModeTaskId === task.id ? 'el-icon-check' : 'el-icon-edit'
-            "
-            circle
-          ></el-button>
-          <el-button
-            @click="dialogVisible = true"
-            type="danger"
-            icon="el-icon-delete"
-            circle
-          ></el-button>
-        </div>
-        <el-dialog :visible.sync="dialogVisible" width="30%">
-          <span>You are sure that you want to delete the task</span>
-          <span slot="footer" class="dialog-footer">
-            <el-button @click="dialogVisible = false">Cancel</el-button>
-            <el-button type="primary" @click="deleteItem(task.id)"
-              >Yes</el-button
-            >
-          </span>
-        </el-dialog>
-      </div>
-    </div>
-  </div>
+	<div class="container">
+		<div class="task-list">
+			<div class="task-list-button-filter">
+				<el-button
+					@click="setSelectedCategory('All')"
+					class="btn-filter"
+					type="success"
+					plain
+					>All</el-button
+				>
+				<el-button
+					@click="setSelectedCategory('Done')"
+					class="btn-filter"
+					type="success"
+					plain
+					>Completed tasks</el-button
+				>
+			</div>
+			<div class="list-item" v-for="(task, index) in filteredList" :key="index">
+				<div class="list-item-task">
+					<h3 class="task-name">
+						<template v-if="editForm.id === task.id">
+							<input v-model="editForm.name" class="edit-input"
+						/></template>
+						<template v-else>{{ task.name }}</template>
+					</h3>
+					<p class="task-description">
+						<template v-if="editForm.id === task.id">
+							<textarea v-model="editForm.description" class="edit-textarea">
+							</textarea>
+						</template>
+						<template v-else>{{ task.description }}</template>
+					</p>
+				</div>
+				<div class="list-item-button">
+					<el-button
+						@click="doneTask(task)"
+						:type="checkTypeButton(task)"
+						icon="el-icon-check"
+						circle
+					>
+					</el-button>
+					<el-button
+						@click="toggleEditMode(task)"
+						:type="editForm.id === task.id ? 'primary' : ''"
+						:icon="
+							editForm.id === task.id ? 'el-icon-check' : 'el-icon-edit'
+						"
+						circle
+					>
+					</el-button>
+					<el-button
+						@click="dialogVisible = true"
+						type="danger"
+						icon="el-icon-delete"
+						circle
+					>
+					</el-button>
+				</div>
+				<el-dialog :visible.sync="dialogVisible" width="30%">
+					<span>You are sure that you want to delete the task</span>
+					<span slot="footer" class="dialog-footer">
+						<el-button @click="dialogVisible = false">Cancel</el-button>
+						<el-button type="primary" @click="deleteItem(task.id)"
+							>Yes</el-button
+						>
+					</span>
+				</el-dialog>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
-import { mapMutations } from 'vuex';
+import { mapState } from "vuex";
+import { mapMutations } from "vuex";
 
 export default {
-  name: 'TaskList',
-  data() {
-    return {
-      dialogVisible: false,
-      editModeTaskId: null,
-      editModeTaskName: '',
-      editModeTaskDescription: '',
-    };
-  },
-  computed: {
-    ...mapState(['taskList']),
-  },
-  methods: {
-    ...mapMutations(['DELETE_TASK', 'DONE_TASK']),
-    toggleEditMode(taskId) {
-      if (this.editModeTaskId === taskId) {
-        this.editModeTaskId = null;
-      } else {
-        this.editModeTaskId = taskId;
+	name: "TaskList",
+	data() {
+		return {
+			dialogVisible: false,
+			editModeTaskId: null,
+			editModeTaskName: "",
+			editModeTaskDescription: "",
+			selectedCategory : "All",
+			editForm: {}
+		};
+	},
+	computed: {
+		...mapState(["taskList"]),
+    filteredList() {
+      if (this.selectedCategory  === 'All') {
+        return this.taskList;
+      } else if (this.selectedCategory  === 'Done') {
+        return this.taskList.filter(task => task.done);
       }
-    },
-    doneTask(task) {
-      this.DONE_TASK(task);
-    },
-    checkTypeButton(task) {
-      if (task.done === true) {
-        return 'success';
-      } else {
-        return 'info';
-      }
-    },
-    deleteItem(id) {
-      this.DELETE_TASK(id);
-      this.dialogVisible = false;
-    },
-  },
+		},
+	},
+	methods: {
+		...mapMutations(["DELETE_TASK", "DONE_TASK"]),
+		setSelectedCategory(category) {
+			this.selectedCategory  = category;
+		},
+		toggleEditMode(task) {
+			if (this.editForm.id === task.id) {
+				
+				this.editForm = {};
+			} else {
+				this.editForm = {...task}
+			}
+		},
+		doneTask(task) {
+			this.DONE_TASK(task);
+		},
+		checkTypeButton(task) {
+			if (task.done === true) {
+				return "success";
+			} else {
+				return "info";
+			}
+		},
+		deleteItem(id) {
+			this.DELETE_TASK(id);
+			this.dialogVisible = false;
+		},
+	},
 };
 </script>
 
 <style scoped>
 .container {
-  width: 100%;
-  max-width: 600px;
-  margin: 0 auto;
+	width: 100%;
+	max-width: 600px;
+	margin: 0 auto;
 }
 .task-list {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 }
 .list-item {
-  width: 100%;
-  margin-bottom: 20px;
-  border: 1px solid rgb(218, 213, 213);
-  box-shadow: 5px -5px 13px rgba(185, 178, 178, 0.466);
-  padding: 10px 30px;
-  box-sizing: border-box;
+	width: 100%;
+	margin-bottom: 20px;
+	border: 1px solid rgb(218, 213, 213);
+	box-shadow: 5px -5px 13px rgba(185, 178, 178, 0.466);
+	padding: 10px 30px;
+	box-sizing: border-box;
 }
 .list-item-button {
-  display: flex;
-  justify-content: flex-end;
+	display: flex;
+	justify-content: flex-end;
 }
 .task-name {
-  font-size: 24px;
-  color: rgb(114, 115, 116);
-  text-transform: uppercase;
+	font-size: 24px;
+	color: rgb(114, 115, 116);
+	text-transform: uppercase;
 }
 .task-description {
-  font-size: 16px;
-  line-height: 28px;
+	font-size: 16px;
+	line-height: 28px;
 }
 .edit-input {
-  width: 100%;
-  font-size: 24px;
-  color: rgb(114, 115, 116);
-  padding: 5px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  height: 40px;
-  text-transform: uppercase;
+	width: 100%;
+	font-size: 24px;
+	color: rgb(114, 115, 116);
+	padding: 5px;
+	border: 1px solid #ccc;
+	border-radius: 4px;
+	height: 40px;
+	text-transform: uppercase;
 }
 .edit-textarea {
-  width: 100%;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  padding: 5px;
-  min-height: 100px;
-  font-size: 16px;
+	width: 100%;
+	border: 1px solid #ccc;
+	border-radius: 4px;
+	padding: 5px;
+	min-height: 100px;
+	font-size: 16px;
+}
+.task-list-button-filter {
+	width: 100%;
+	display: flex;
+	justify-content: end;
+	margin-bottom: 10px;
 }
 </style>
